9d0826c2b432a640ebfcdb405e8aeaea4bba5518
Boussad <boussad@gmail.fr>
2022-10-24 14:04:22 +0200

    second modification of main.tf

--- main.tf
+++ main.tf
@@ -8,14 +8,13 @@ module "my_vpc" {
     vpc_cidr                = "10.0.0.0/16"
     public_subnets_cidr     = ["10.0.1.0/24", "10.0.2.0/24"]
     private_subnets_cidr    = ["10.0.3.0/24", "10.0.4.0/24"]
-    availability_zone       = ["us-west-2a", "us-west-2b"]
+    availability_zone       = ["us-west-1c", "us-west-1b"]
     name                    = "boussad_vpc"
 }
 
 module "my_s3" {
     source                  = "./modules/S3"
     bucket_name             = "boussad-S3"
-    path_folder_content     = "./src/"
 }
 
 module "my_ec2_role_allow_s3" {

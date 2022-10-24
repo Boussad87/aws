e757f1a22bbcaaad138d1d9d5d1f402f9b87a6bc
Boussad ARROUA <99990110+Boussad87@users.noreply.github.com>
2022-10-24 15:16:21 +0200

    Update main.tf of s3

--- main.tf
+++ main.tf
@@ -6,19 +6,16 @@ provider "aws" {
 module "my_vpc" {
     source                  = "./modules/vpc"
     vpc_cidr                = "10.0.0.0/16"
-    public_subnets_cidr     = ["10.0.1.0/24", "10.0.2.0/24"]
-    private_subnets_cidr    = ["10.0.3.0/24", "10.0.4.0/24"]
-    availability_zone       = ["us-west-1c", "us-west-1b"]
-    name                    = "boussad_vpc"
+    public_subnet_cidr     = ["10.0.1.0/24", "10.0.2.0/24"]
+    private_subnet_cidr    = ["10.0.3.0/24", "10.0.4.0/24"]
+    
 }
 
 module "my_s3" {
     source                  = "./modules/S3"
-    bucket_name             = "boussad-S3"
+  
 }
 
 module "my_ec2_role_allow_s3" {
     source                  = "./modules/ec2_role_S3"
-    name                    = "devops"
-    bucket_name             = "boussad-S3"
 }

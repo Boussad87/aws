2629890788add66213c60b2e3479a94bc2ead592
Boussad ARROUA <99990110+Boussad87@users.noreply.github.com>
2022-10-24 15:36:24 +0200

    Update vars.tf

--- modules/vpc/vars.tf
+++ modules/vpc/vars.tf
@@ -4,15 +4,15 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnet_cidr" {
-  type = list(number)
+  type = list
   default= ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidr" {
-  type = list(number)
+  type = list
   default= ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "availability_zone_names" {
-  type    = list(string)
+  type    = list
   default = ["us-west-1c", "us-west-1b"]
